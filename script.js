24afc22f9d02e8e4fcb5e0463ef2be017169173e
alexrh0 <144929437+alexrh0@users.noreply.github.com>
2023-09-16 19:20:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const totalImages = 86;
 const randomIndex = Math.floor(Math.random() * totalImages) + 1;
-const randomImage = `imagen${randomIndex}.jpg`;
+const randomImage = `imagen${randomIndex}.jpeg`;
 
 document.getElementById('random-image').src = randomImage;
